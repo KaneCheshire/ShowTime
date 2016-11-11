ac2ac53f0f1206747d53b398dc105d8c74baee8f
Kane Cheshire <kane.cheshire@googlemail.com>
2016-11-11 08:56:13 +0000

    Initial Commit

---
+++ ShowTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShowTime
+//
+//  Created by Kane Cheshire on 11/11/2016.
+//  Copyright © 2016 Kane Cheshire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
