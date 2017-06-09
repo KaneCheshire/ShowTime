79ca6117cbcd7a928174802016d0c3900386ef5e
KaneCheshire <contactme@kanecheshire.com>
2017-06-09 22:08:23 +0100

    Updated example project

--- Example/ShowTime/AppDelegate.swift
+++ Example/ShowTime/AppDelegate.swift
@@ -7,25 +7,18 @@
 //
 
 import UIKit
-// You only need to import this if changing any default values
-// ShowTime v1 will work without having to import!
 import ShowTime
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
   var window: UIWindow?
-
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
   
-/* Uncomment to disable ShowTime */
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     
-    // ShowTime.enabled = .never
+    ShowTime.enabled = .always
     
-    
-    
-    
-/* Uncomment to modify ShowTime display settings */
+    /* Uncomment to modify ShowTime display settings */
     
     // ShowTime.disappearAnimation = .scaleDown
     // ShowTime.shouldIgnoreApplePencilEvents = false
--- ShowTime.swift
+++ ShowTime.swift
@@ -151,7 +151,7 @@ class TouchView: UILabel {
 internal var _touches = [UITouch : TouchView]()
 
 extension UIWindow {
-
+  
   fileprivate class func swizzle() { // `initialize()` removed in Swift 4
     struct Swizzled { static var once = false } // Workaround for missing dispatch_once in Swift 3
     guard !Swizzled.once else { return }
