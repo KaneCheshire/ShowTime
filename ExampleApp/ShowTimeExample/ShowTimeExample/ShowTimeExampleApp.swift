0627790a97c1bd8e9aec8af1eeec00db58c6b554
Kane Cheshire <kane.cheshire@googlemail.com>
2023-01-14 09:16:51 +0000

    Added new example Xcode project

---
+++ ExampleApp/ShowTimeExample/ShowTimeExample/ShowTimeExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowTimeExampleApp.swift
+//  ShowTimeExample
+//
+//  Created by Kane Cheshire on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShowTimeExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
