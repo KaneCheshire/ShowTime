8dba918007cbe6cae1323d7ea3dcb10f25cc981d
Kane Cheshire <kane.cheshire@googlemail.com>
2023-01-15 21:24:09 +0100

    Improved examples with code examples

--- ExampleApp/ShowTimeExample/ShowTimeExample/ContentView.swift
+++ ExampleApp/ShowTimeExample/ShowTimeExample/ContentView.swift
@@ -13,6 +13,12 @@ import ShowTime
 // You don't even need to `import ShowTime` to get going, but if you want to
 // change any options you can just `import ShowTime` and have some fun with it.
 
+private let numberFormatter: NumberFormatter = {
+    let formatter = NumberFormatter()
+    formatter.maximumFractionDigits = 2
+    return formatter
+}()
+
 struct ContentView: View {
     @State
     private var enabled: ShowTime.Enabled = ShowTime.enabled
@@ -34,79 +40,121 @@ struct ContentView: View {
     private var multipleTapCountTextColor: CGColor = ShowTime.multipleTapCountTextColor.cgColor
     @State
     private var ignoreApplePencilEvents: Bool = ShowTime.shouldIgnoreApplePencilEvents
+    @State
+    private var copied: Bool = false
+    @State
+    private var workItem: DispatchWorkItem? {
+        willSet {
+            workItem?.cancel()
+        }
+    }
     
     var body: some View {
         ScrollView {
+            Text("ShowTime works with SwiftUI and UIKit, you don't even need to import ShowTime if you're happy with the default options!")
+                .bold()
             VStack(alignment: .leading) {
-                Text("ShowTime works with SwiftUI and UIKit, you don't even need to import ShowTime if you're happy with the default options!")
-                    .bold()
-                Divider()
-                VStack(alignment: .leading) {
-                    Row(title: "Enabled", description: "When and if ShowTime is enabled.") {
-                        Menu("\(enabled.stringValue)") {
-                            ForEach(ShowTime.Enabled.allCases, id: \.self) { enabled in
-                                Button(enabled.stringValue) { ShowTime.enabled = enabled }
-                            }
+                Row(
+                    title: "Enabled",
+                    description: "When and if ShowTime is enabled.",
+                    codeExample: "ShowTime.enabled = \(enabled.stringValue)"
+                ) {
+                    Picker("\(enabled.stringValue)", selection: $enabled) {
+                        ForEach(ShowTime.Enabled.allCases, id: \.self) { enabled in
+                            Button(enabled.stringValue) { self.enabled = enabled }
                         }
-                    }
-                    
-                    Row(title: "Animation", description: "The animation to perform to hide touch indicators.") {
-                        Menu("\(disappearAnimation.stringValue)") {
-                            ForEach(Animation.allCases, id: \.self) { animation in
-                                Button(animation.stringValue) { ShowTime.disappearAnimation = animation.mapped }
-                            }
-                        }
-                    }
-                    
-                    Row(title: "Animation delay", description: "The delay until the disappearing animation is performed.") {
-                        Picker("\(disappearDelay)", selection: $disappearDelay) {
-                            ForEach([0.1, 0.2, 0.5, 1.0], id: \.self) {
-                                Text("\($0)")
-                            }
-                        }
-                    }
-                    
-                    Row(title: "Stroke color", description: "The color of the stroke around touch indicators.") {
-                        ColorPicker("", selection: $strokeColor, supportsOpacity: true)
-                    }
-                    
-                    Row(title: "Stroke width", description: "The width of the stroke around touch indicators.") {
-                        Picker("\(strokeWidth)", selection: $strokeWidth) {
-                            ForEach(0 ..< 51) {
-                                Text("\($0)")
-                            }
-                        }
-                    }
-                    
-                    Row(title: UIColor(cgColor: fillColor) == .auto ? "Fill color (auto)" : "Fill color (explicit)", description: "The fill color of touch indicators. When set to .auto (the default) then the fill color is dervied from the stroke color.") {
-                        ColorPicker("", selection: $fillColor, supportsOpacity: true)
-                    }
-                    
-                    Row(title: "Size", description: "The size of touch indicators.") {
-                        Picker("\(size)", selection: $size) {
-                            ForEach(1 ..< 101) {
-                                Text("\($0)").tag($0)
-                            }
-                        }
-                    }
-                    
-                    Row(title: "Multiple tap count", description: "Whether the number of taps is shown in touch indicators when tapping multiple times in one place (like a double tap).") {
-                        Toggle(isOn: $showMultipleTapCount) {
-                            Text("")
-                        }
-                    }
-                    
-                    Row(title: "Multiple tap text color", description: "The text color of the text to use when multiple taps is shown.") {
-                        ColorPicker("", selection: $multipleTapCountTextColor, supportsOpacity: true)
-                    }
-                    
-                    Row(title: "Ignore Apple Pencil events", description: "Whether touch indicators should be hidden for Apple Pencil events.") {
-                        Toggle(isOn: $ignoreApplePencilEvents) { Text("") }
                     }
                 }
+
+                Row(
+                    title: "Animation",
+                    description: "The animation to perform to hide touch indicators.",
+                    codeExample: "ShowTime.animation = \(disappearAnimation.stringValue)"
+                ) {
+                    Picker("\(disappearAnimation.stringValue)", selection: $disappearAnimation) {
+                        ForEach(Animation.allCases, id: \.self) { animation in
+                            Button(animation.stringValue) { disappearAnimation = animation }
+                        }
+                    }
+                }
+
+                Row(
+                    title: "Animation delay",
+                    description: "The delay until the disappearing animation is performed.",
+                    codeExample: "ShowTime.disappearDelay = \(numberFormatter.string(from: disappearDelay as NSNumber)!)"
+                ) {
+                    VStack(alignment: .trailing) {
+                        Text(disappearDelay, format: .number)
+                        Stepper("\(disappearDelay)", value: $disappearDelay, in: 0 ... 10, step: 0.1)
+                    }
+                }
+
+                Row(
+                    title: "Stroke color",
+                    description: "The color of the stroke around touch indicators.",
+                    codeExample: "ShowTime.strokeColor = \(strokeColor.uicolorCodeExample)"
+                ) {
+                    ColorPicker("", selection: $strokeColor, supportsOpacity: true)
+                }
+
+                Row(
+                    title: "Stroke width",
+                    description: "The width of the stroke around touch indicators.",
+                    codeExample: "ShowTime.strokeWidth = \(numberFormatter.string(from: strokeWidth as NSNumber)!)"
+                ) {
+                    VStack(alignment: .trailing) {
+                        Text(strokeWidth, format: .number)
+                        Stepper("\(strokeWidth)", value: $strokeWidth, in: 0 ... 100, step: 1)
+                    }
+                }
+
+                Row(
+                    title: UIColor(cgColor: fillColor) == .auto ? "Fill color (auto)" : "Fill color (explicit)",
+                    description: "The fill color of touch indicators. When set to .auto (the default) then the fill color is dervied from the stroke color.",
+                    codeExample: "ShowTime.fillColor = \(fillColor.uicolorCodeExample)"
+                ) {
+                    ColorPicker("", selection: $fillColor, supportsOpacity: true)
+                }
+
+                Row(
+                    title: "Size",
+                    description: "The size of touch indicators.",
+                    codeExample: "ShowTime.size = CGSize(width: \(Int(size)), height: \(Int(size)))"
+                ) {
+                    VStack(alignment: .trailing) {
+                        Text(size, format: .number)
+                        Stepper("\(size)", value: $size, in: 0 ... 100, step: 1)
+                    }
+                }
+
+                Row(
+                    title: "Multiple tap count",
+                    description: "Whether the number of taps is shown in touch indicators when tapping multiple times in one place (like a double tap).",
+                    codeExample: "ShowTime.shouldShowMultipleTapCount = \(showMultipleTapCount)"
+                ) {
+                    Toggle(isOn: $showMultipleTapCount) {
+                        Text("")
+                    }
+                }
+
+                Row(
+                    title: "Multiple tap text color",
+                    description: "The text color of the text to use when multiple taps is shown.",
+                    codeExample: "ShowTime.multipleTapCountTextColor = \(multipleTapCountTextColor.uicolorCodeExample)"
+                ) {
+                    ColorPicker("", selection: $multipleTapCountTextColor, supportsOpacity: true)
+                }
+
+                Row(
+                    title: "Ignore Apple Pencil events",
+                    description: "Whether touch indicators should be hidden for Apple Pencil events.",
+                    codeExample: "ShowTime.shouldIgnoreApplePencilEvents = \(ignoreApplePencilEvents)"
+                ) {
+                    Toggle(isOn: $ignoreApplePencilEvents) { Text("") }
+                }
             }
+            .padding()
         }
-        .padding()
         .onChange(of: enabled) { ShowTime.enabled = $0 }
         .onChange(of: disappearAnimation) { ShowTime.disappearAnimation = $0.mapped }
         .onChange(of: disappearDelay) { ShowTime.disappearDelay = $0 }
@@ -117,6 +165,28 @@ struct ContentView: View {
         .onChange(of: showMultipleTapCount) { ShowTime.shouldShowMultipleTapCount = $0 }
         .onChange(of: multipleTapCountTextColor) { ShowTime.multipleTapCountTextColor = UIColor(cgColor: $0) }
         .onChange(of: ignoreApplePencilEvents) { ShowTime.shouldIgnoreApplePencilEvents = $0 }
+        .onReceive(NotificationCenter.default.publisher(for: .init("ShowTime.AddedToPasteboard"))) { _ in
+            withAnimation {
+                copied = true
+            }
+            let item = DispatchWorkItem {
+                withAnimation {
+                    copied = false
+                }
+            }
+            workItem = item
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: item)
+        }
+        .overlay {
+            Text("Copied to clipboard!")
+                .frame(width: 200)
+                .padding()
+                .foregroundColor(Color.white)
+                .background(Color.gray)
+                .cornerRadius(16)
+                .shadow(radius: 10)
+                .opacity(copied ? 1 : 0)
+        }
     }
 }
 
@@ -157,6 +227,37 @@ private extension ShowTime.Enabled {
         case .never: return ".never"
         case .always: return ".always"
         case .debugOnly: return ".debugOnly"
+        }
+    }
+}
+
+private extension CGColor {
+    var rgba: (r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
+        let color = UIColor(cgColor: self)
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        let rP: UnsafeMutablePointer<CGFloat> = withUnsafeMutablePointer(to: &r) { $0 }
+        let gP: UnsafeMutablePointer<CGFloat> = withUnsafeMutablePointer(to: &g) { $0 }
+        let bP: UnsafeMutablePointer<CGFloat> = withUnsafeMutablePointer(to: &b) { $0 }
+        let aP: UnsafeMutablePointer<CGFloat> = withUnsafeMutablePointer(to: &a) { $0 }
+
+        assert(color.getRed(rP, green: gP, blue: bP, alpha: aP))
+        return (r, g, b, a)
+    }
+    
+    var uicolorCodeExample: String {
+        let color = UIColor(cgColor: self)
+        if color == .auto {
+            return ".auto"
+        } else {
+            let computedRGBA = rgba
+            let formattedR = numberFormatter.string(from: computedRGBA.r as NSNumber)!
+            let formattedG = numberFormatter.string(from: computedRGBA.g as NSNumber)!
+            let formattedB = numberFormatter.string(from: computedRGBA.b as NSNumber)!
+            let formattedA = numberFormatter.string(from: computedRGBA.a as NSNumber)!
+            return "UIColor(red: \(formattedR), green: \(formattedG), blue: \(formattedB), alpha: \(formattedA))"
         }
     }
 }
